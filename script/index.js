a17373d51f9c4a7750302e17123eeedeae110a44
hriday333 <zakirhasanhriday@gmaik.com>
2025-09-06 14:21:51 +0600

    added part 1

--- script/index.js
+++ script/index.js
@@ -164,3 +164,18 @@ const displayLesson = (lessons) => {
 }
 loadLesson();
 
+document.getElementById("btn-search").addEventListener("click", ()=>{
+    removeActive();
+    const input = document.getElementById("input-search");
+    const searchValue =input.value.trim().toLowerCase();
+    console.log(searchValue)
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res)=>res.json())
+    .then((data)=>{
+        const allWords = data.data;
+        console.log(allWords);
+        const filterWords = allWords.filter(word=>word.word.toLowerCase().includes(searchValue));
+        displayWord(filterWords)
+    })
+})
+
